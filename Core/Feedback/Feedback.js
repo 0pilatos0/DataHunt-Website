5b61a538f0bbec6af20715c3115e3791dbb0e65e
jan <jangarretsen01@gmail.com>
2021-10-07 10:22:45 +0200

    added userfeedback for the website

---
+++ Core/Feedback/Feedback.js
@@ -0,0 +1,22 @@
+module.exports = class Feedback{
+
+    constructor() {
+
+    }
+
+    static ShowFeedback(enumType, message, timeout = enumType.DEFAULT_TIMEOUT){
+        return `<script> let div = document.createElement("div");
+        div.id = "${enumType.TYPE}";
+        div.classList.add("${enumType.COLOR}");
+        document.body.appendChild(div);
+
+        let text = document.createElement("p");
+        text.innerHTML = "${message}";
+        div.appendChild(text);
+
+        setTimeout(function(){
+            document.body.removeChild(div);
+        }, ${timeout}*1000);</script>`
+    }
+
+};
